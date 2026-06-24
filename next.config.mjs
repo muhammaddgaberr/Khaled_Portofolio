a5f4ebb2ada7053c58f338da466c88d93afca007
muhammaddgaberr <muhammaddgaberr@gmail.com>
2026-06-25 00:58:22 +0300

    fixing css

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  basePath: '/Khaled_Portofolio',
+  assetPrefix: '/Khaled_Portofolio',
   typescript: {
     ignoreBuildErrors: true,
   },
